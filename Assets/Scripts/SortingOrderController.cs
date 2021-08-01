7d2544f50c14df61be25563dbec4042f0ac7b309
BrandonMRendon <69318653+BrandonMRendon@users.noreply.github.com>
2021-07-31 20:44:08 -0800

    File dump. Init commit

---
+++ Assets/Scripts/SortingOrderController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingOrderController : LayerContainer
+{
+    public bool Level1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            ColliderTilesLevelOneOnly.SetActive(Level1);
+            ColliderTilesLevelTwoOnly.SetActive(!Level1);
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = Level1 ? LAYER_One : LAYER_Two;
+
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
