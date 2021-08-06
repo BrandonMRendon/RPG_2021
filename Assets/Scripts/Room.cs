9988743d83c044cb850d99c621247d65135ad2a2
Ryan Anderson <ryananderson352@yahoo.com>
2021-08-05 20:11:50 -0700

    Camera Update
    -Added Room script (keeps track of enemy/item activation and camera)
    -Added cinemachine brain to main camera
    -Created 4 test rooms with cameras and polygon collider triggers

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    //public Enemy[] enemies; keep track?
+    //public item[] items;
+
+    public GameObject virtualCamera;
+
+    public virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+
+            //for loop with enemy activation
+
+            //for loop with item activation
+
+
+            virtualCamera.SetActive(true);
+        }
+    }
+
+    public virtual void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+
+            //for loop with enemy deactivation
+
+            //for loop with item deactivation
+
+
+            virtualCamera.SetActive(false);
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
